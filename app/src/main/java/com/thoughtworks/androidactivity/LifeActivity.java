bd5c1e076f4f52271a063d767aed157d6d243758
Jerry777777 <491818175@qq.com>
2020-08-14 03:04:13 +0800

    [xiawenhao] feat: complete the LifeActivity

--- app/src/main/java/com/thoughtworks/androidactivity/LifeActivity.java
+++ app/src/main/java/com/thoughtworks/androidactivity/LifeActivity.java
@@ -3,12 +3,50 @@ package com.thoughtworks.androidactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 
 public class LifeActivity extends AppCompatActivity {
+    public final String TAG = LifeActivity.this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.life);
+        Log.d(TAG, "onCreate");
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
 }
